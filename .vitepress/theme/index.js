981d55fa0589d535637e9ab6921659fff275f374
Anton Medvedev <anton@medv.io>
2026-06-08 11:20:26 +0200

    Add MamlHero component and configure Google Fonts preloading

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,9 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import Playground from '../components/Playground.vue'
+import MamlHero from './MamlHero.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('Playground', Playground)
+    app.component('MamlHero', MamlHero)
   },
 }
